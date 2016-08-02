13c9460ed62c337b566de037ed0b8b06fc676038
RaIN <razor13372k@gmail.com>
2016-08-02 15:44:19 +0200

    Changed login.php

--- login.php
+++ login.php
@@ -1,11 +1,13 @@
 <?php 
     $_db_host = "localhost";
-    $_db_datenbank = ""; 
+    $_db_database = ""; 
     $_db_username = ""; 
-    $_db_passwort = ""; 
+    $_db_password = ""; 
+
+    SESSION_START();
 
     # Connect to Database
-    $con = mysqli_connect($_db_host, $_db_username, $_db_passwort);
+    $con = mysqli_connect($_db_host, $_db_username, $_db_password);
 
     # Connection Successful ?
     if (mysqli_connect_errno())
@@ -15,72 +17,72 @@
         }
         
     # Connect to the Right Database 
-    $datenbank = mysqli_select_db($_db_datenbank, $con); 
+    $database = mysqli_select_db($con, $_db_database); 
 
-    if (!$datenbank) 
+    if (!$database) 
         { 
         echo "Cant use the Database: " . mysqli_error(); 
         mysql_close($con);        # Close Database
-        exit;                    # Exit Programm
+        exit;                     # Exit Programm
         } 
 
 ##################################################################
 
-# Check $_POST
-if(isset($_POST["submit"]))
-    {
-    $_username = mysql_real_escape_string($_POST["username"]); 
-    $_passwort = mysql_real_escape_string($_POST["passwort"]); 
-
-    $sel_user = "SELECT * FROM login_usernamen WHERE
-                    username='$_username' AND
-                    passwort='$_passwort' AND
-                    user_geloescht=0
-                LIMIT 1";
-
-    # Check if User exists in Database
-    $run_user = mysqli_query($con, $sel_user);
-    $check_user = mysqli_num_rows($run_user);
-
-
-    if($check_user > 0)
-    {
-    echo "Login Successful.<br>";
-
-    $_SESSION[‘username’]=$_username;
-
-    # Save in Session that User is logged in !
-    $_SESSION["login"] = 1;
-
-    # Save User Session !
-    $_SESSION["user"] = mysqli_fetch_array($run_user, MYSQLI_ASSOC);
-
-    # Set Login Date in Database !
-    $sel_user = "UPDATE login_usernamen SET letzter_login=NOW()
-                 WHERE id=".$_SESSION["user"]["id"];
-                 mysql_query($sel_user);
-                 }
-             else
-                 {
-                 echo "Wrong Login details.<br>";
-                 }
-             } 
-    # User Logged in ???
-    if ($_SESSION["login"] == 0)
+    # Check $_POST
+    if(isset($_POST["submit"]))
         {
-        # Not Logged in, close the database
-        # and exit the programm
-        include("index.html");
-        mysql_close($con);
-        exit;
-        }
+        $_username = mysql_real_escape_string($_POST["username"]); 
+        $_passwort = mysql_real_escape_string($_POST["passwort"]); 
 
-        # If the User is Logged in
-        # Show this Code
-        echo "Login Successful!<br>";
+        $sel_user = "SELECT * FROM login_usernamen WHERE
+                        username='$_username' AND
+                        passwort='$_passwort' AND
+                        user_geloescht=0
+                    LIMIT 1";
+
+        # Check if User exists in Database
+        $run_user = mysqli_query($con, $sel_user);
+        $check_user = mysqli_num_rows($run_user);
+
+
+        if($check_user > 0)
+        {
+        echo "Login Successful.<br>";
+
+        $_SESSION[‘username’]=$_username;
+
+        # Save in Session that User is logged in !
+        $_SESSION["login"] = 1;
+
+        # Save User Session !
+        $_SESSION["user"] = mysqli_fetch_array($run_user, MYSQLI_ASSOC);
+
+        # Set Login Date in Database !
+        $sel_user = "UPDATE login_usernamen SET letzter_login=NOW()
+                    WHERE id=".$_SESSION["user"]["id"];
+                    mysql_query($sel_user);
+                    }
+                else
+                    {
+                     echo "Wrong Login details.<br>";
+                    }
+                } 
+        # User Logged in ???
+        if ($_SESSION["login"] == 0)
+            {
+            # Not Logged in, close the database
+            # and exit the programm
+            include("index.html");
+            mysql_close($con);
+            exit;
+            }
+
+            # If the User is Logged in
+            # Show this Code
+            echo "Login Successful!<br>";
 
 ##################################################################
 
-        # Close the Database again
-        mysql_close($con);
+            # Close the Database again
+            mysql_close($con);
 ?>
